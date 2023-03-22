011c2bbc70c6fe27523c774c9ef72834d50b6713
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-03-22 16:21:52 -0700

    file path additions

---
+++ script.js
@@ -0,0 +1,57 @@
+// !!!!!!!!!!!JAVASCRIPT!!!!!!!!!!!!!
+
+// Basic Commands
+// alert();
+// console.log()
+
+// prompt()
+
+
+
+
+
+// let <-- Yes
+// const <-- Yes
+// var <-- NO
+// (nothing) <-- NO
+
+
+// example of declaring a CHANGEABLE VARIABLE
+// const myName = `iAmAndrewCarroll`
+// document.write(myName);
+
+// myName = `AndrewCarroll`;
+// document.write(myName);
+
+// DATA TYPES 
+// strings --> TEXT --> Surrounded in quotes: `iAmAndrewCarroll`, `2`, `two`
+// number --> 2, 42, 120
+// boolean --> TRUE or FALSE
+
+// let myFavColor = 'blue';
+// let myOtherFavColor = 'white';
+
+// let num;
+// num = 42; 
+
+// let thisNum = 24;
+
+let usersName = prompt("What is your email address?"); 
+console.log(usersName;)
+
+// conditional logic basic structure
+// if (if this is true){execute the code in these brackets}
+// if (){
+//    execute the code in these brackets
+// }
+
+if (usersName == 'iAmAndrewCarroll'){
+    document.write('Hello my Savage Poet')
+} else if (usersName == 'Justin'){
+    document.write("TA's are AMAZING");
+else if (usersName == 'Kjell'){
+    document.write("TA's are AMAZING");
+} else{
+    document.write("I guess you can come inside...");
+}
+}
