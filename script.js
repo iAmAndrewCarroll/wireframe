c3653f476bcb533cbe5996d1612ad3cd86fa87b6
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-03-22 20:20:29 -0700

    updated javaScripts

--- script.js
+++ script.js
@@ -36,8 +36,38 @@
 
 // let thisNum = 24;
 
-let usersName = prompt("What is your email address?"); 
-console.log(usersName;)
+function getUsername(){
+    let mydiv=document.getElementById("divine")
+    let userName = prompt("What is your government name?"); 
+    console.log(userName);
+    mydiv.innerHTML=userName;
+    if (userName == 'iAmAndrewCarroll'){
+    mydiv.innerHTML=userName + ", hello my Savage Poet";
+    } else if (userName === 'Justin'){
+    mydiv.innerHTML=userName + ", you probably aren't being followed";
+    } else if (userName == 'Kjell'){
+    mydiv.innerHTML="All Hail kJ-E-L-L-O!!!";
+    } else if (userName == 'Toni'){
+    mydiv.innerHTML="You are so loved Sweet Sister!"
+    } else if (userName == 'Kassie'){
+    mydiv.innerHTML="You are doing such a great job!"
+    }
+    else {
+    mydiv.innerHTML="They are definitely watching you...";
+    // mydiv.innerHTML=userName;
+    }
+} 
+getUsername()
+
+function getAffirmation(){
+    let mydiv=document.getElementById("affirmation")
+if (confirm("Did you know that you are doing amazing things?")) {
+    mydiv.innerHTML=("Take a moment and breathe in that feeling of well-being.");
+} else {
+    mydiv.innerHTML=("You are. Give yourself a moment to appreciate all the hard work you do.");
+}
+}
+getAffirmation()
 
 // conditional logic basic structure
 // if (if this is true){execute the code in these brackets}
@@ -45,13 +75,13 @@ console.log(usersName;)
 //    execute the code in these brackets
 // }
 
-if (usersName == 'iAmAndrewCarroll'){
-    document.write('Hello my Savage Poet')
-} else if (usersName == 'Justin'){
-    document.write("TA's are AMAZING");
-else if (usersName == 'Kjell'){
-    document.write("TA's are AMAZING");
-} else{
-    document.write("I guess you can come inside...");
-}
-}
+// if (userName == 'iAmAndrewCarroll'){
+    // document.write('Hello my Savage Poet')
+// } else if (userName == 'Justin'){
+    // document.write("TA's are AMAZING");
+// } else if (userName == 'Kjell'){
+    // document.write("TA's are AMAZING");
+// } else{
+    // document.write("They are definitely tracking you...");
+// }
+
